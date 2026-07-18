9de0f264194cd5fcca5528832e49cc6d97bbd604
mayanknaruka <mayanknaruka47@gmail.com>
2026-07-18 23:56:33 +0530

    js file was done also

--- script.js
+++ script.js
@@ -1,80 +1,188 @@
 const Gameboard = (() => {
+
     const board = ["", "", "", "", "", "", "", "", ""];
 
     function placeMark(index, mark) {
+
         if (board[index] !== "") {
             return false;
         }
 
         board[index] = mark;
+
         return true;
+
     }
 
     function getBoard() {
         return board;
     }
 
+    function resetBoard() {
+
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+
+    }
+
     return {
         placeMark,
         getBoard,
+        resetBoard,
     };
+
 })();
 
 function Player(name, mark) {
+
     return {
         name,
         mark,
     };
+
 }
 
 const gameController = (() => {
 
-    const playerOne = Player("Player 1", "X");
-    const playerTwo = Player("Player 2", "O");
+    let playerOne = Player("Player 1", "X");
+    let playerTwo = Player("Player 2", "O");
 
     let currentPlayer = playerOne;
 
+    let gameOver = false;
+
+    const winningCombinations = [
+
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        [0, 4, 8],
+        [2, 4, 6]
+
+    ];
+
     function switchPlayer() {
-        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+
+        currentPlayer =
+            currentPlayer === playerOne
+                ? playerTwo
+                : playerOne;
+
     }
 
     function checkWinner() {
+
         const board = Gameboard.getBoard();
 
-        // Step 5
+        for (let combination of winningCombinations) {
+
+            const [a, b, c] = combination;
+
+            if (
+                board[a] !== "" &&
+                board[a] === board[b] &&
+                board[b] === board[c]
+            ) {
+
+                return true;
+
+            }
+
+        }
+
         return false;
+
     }
 
     function checkTie() {
+
         const board = Gameboard.getBoard();
 
         return board.every(cell => cell !== "");
+
     }
 
     function playRound(index) {
 
+        if (gameOver) return;
+
         if (!Gameboard.placeMark(index, currentPlayer.mark)) {
-            console.log("Cell already occupied!");
             return;
         }
 
-        displayController.render();
-
         if (checkWinner()) {
-            console.log(currentPlayer.name + " wins!");
+
+            gameOver = true;
+
+            displayController.render();
+
+            displayController.updateMessage(
+                currentPlayer.name + " Wins!"
+            );
+
             return;
+
         }
 
         if (checkTie()) {
-            console.log("It's a tie!");
+
+            gameOver = true;
+
+            displayController.render();
+
+            displayController.updateMessage(
+                "It's a Tie!"
+            );
+
             return;
+
         }
 
         switchPlayer();
+
+        displayController.render();
+
+        displayController.updateMessage(
+            currentPlayer.name + "'s Turn"
+        );
+
+    }
+
+    function startGame(player1Name, player2Name) {
+
+        playerOne = Player(
+            player1Name || "Player 1",
+            "X"
+        );
+
+        playerTwo = Player(
+            player2Name || "Player 2",
+            "O"
+        );
+
+        currentPlayer = playerOne;
+
+        gameOver = false;
+
+        Gameboard.resetBoard();
+
+        displayController.render();
+
+        displayController.updateMessage(
+            currentPlayer.name + "'s Turn"
+        );
+
     }
 
     return {
         playRound,
+        startGame,
     };
 
 })();
@@ -82,6 +190,8 @@ const gameController = (() => {
 const displayController = (() => {
 
     const boardContainer = document.getElementById("gameboard");
+    const message = document.getElementById("message");
+    const startBtn = document.getElementById("startBtn");
 
     function render() {
 
@@ -97,10 +207,10 @@ const displayController = (() => {
 
             square.textContent = cell;
 
-            square.dataset.index = index;
-
             square.addEventListener("click", () => {
+
                 gameController.playRound(index);
+
             });
 
             boardContainer.appendChild(square);
@@ -109,10 +219,33 @@ const displayController = (() => {
 
     }
 
+    function updateMessage(text) {
+
+        message.textContent = text;
+
+    }
+
+    startBtn.addEventListener("click", () => {
+
+        const player1Name = document.getElementById("player1").value;
+
+        const player2Name = document.getElementById("player2").value;
+
+        gameController.startGame(player1Name, player2Name);
+
+        startBtn.textContent = "Restart Game";
+
+    });
+
     return {
+
         render,
+        updateMessage,
+
     };
 
 })();
 
 displayController.render();
+
+displayController.updateMessage("Click Start Game");
