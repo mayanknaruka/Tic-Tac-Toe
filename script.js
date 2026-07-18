814e57f06465f16f5628a592a47154d7856ecc4c
mayanknaruka <mayanknaruka47@gmail.com>
2026-07-18 22:45:46 +0530

    working in step 4

--- script.js
+++ script.js
@@ -1,37 +1,88 @@
 const Gameboard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
 
+    function placeMark(index, mark) {
+        board[index] = mark;
+    }
+
+    function getBoard() {
+        return board;
+    }
+
     return {
         board,
+        placeMark,
+        getBoard,
     };
 
 })();
 
-function Player(name, marks){
-
-    return{
+function Player(name, mark) {
+    return {
         name,
         mark,
-    }
+    };
 }
 
-const gameController = ( ()=>{
-    const Playerone = Player("player 1", "X")
-    const playertwo = Player("Player 2", "O")
+const gameController = (() => {
 
-    let currentPlayer = Playerone
+    const playerOne = Player("Player 1", "X");
+    const playerTwo = Player("Player 2", "O");
 
-    return{
-        Playerone,
-        playertwo,
-        currentPlayer
+    let currentPlayer = playerOne;
+
+    function switchPlayer() {
+        if (currentPlayer === playerOne) {
+            currentPlayer = playerTwo;
+        } else {
+            currentPlayer = playerOne;
+        }
     }
 
-})
+    function checkWinner() {
+        const board = Gameboard.getBoard();
 
-const displayController = ( ()=>{
+        return false;
+    }
+
+    function checkTie() {
+        const board = Gameboard.getBoard();
+
+        return board.every(cell => cell !== "");
+    }
+
+    function playRound(index) {
+        Gameboard.placeMark(index, currentPlayer.mark);
+
+        if (checkWinner()) {
+            console.log(currentPlayer.name + " wins!");
+            return;
+        }
+
+        if (checkTie()) {
+            console.log("It's a tie!");
+            return;
+        }
+
+        switchPlayer();
+    }
 
     return {
-        
-    }
-} )
+        playRound,
+    };
+
+})();
+
+const displayController = (() => {
+
+    return {
+
+    };
+
+})();
+
+gameController.playRound(0);
+gameController.playRound(4);
+gameController.playRound(1);
+gameController.playRound(5);
+gameController.playRound(2);
